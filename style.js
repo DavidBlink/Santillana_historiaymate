1f3268a91af30e63bb2e10c5d21dd24b430bc74f
DavidBlink <39801596+DavidBlink@users.noreply.github.com>
2018-12-03 17:03:32 +0100

    Update style.js

--- style.js
+++ style.js
@@ -79,6 +79,7 @@
 				{ name: 'Caja Actividades morado', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-26' } },
 				{ name: 'Caja título gris', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-2' } },
 				{ name: 'Caja Fondo blanco', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-5' } },
+				{ name: 'Caja azul-claro', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-6' } },
 				{ name: 'titu morado Fondo blanco', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-15' } },
 				{ name: 'titu formacion Fondo blanco', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-32' } },
 				{ name: 'Caja comprensión', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-8' } },
